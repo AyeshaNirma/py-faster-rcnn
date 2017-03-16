ee5938be7eaec85601f674f1dc7707c445b96d9a
hanzhipeng <hanzhipeng@pku.edu.cn>
2017-03-16 19:27:21 +0800

    change datasets folder to point to 'bottle'
    Signed-off-by: hanzhipeng <hanzhipeng@pku.edu.cn>
    
    After change the path, recompile the pascal_voc to generate
    pascal_voc.pyc

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -26,13 +26,12 @@ class pascal_voc(imdb):
         self._image_set = image_set
         self._devkit_path = self._get_default_path() if devkit_path is None \
                             else devkit_path
-        self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+        # self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+        self._data_path = os.path.join(self._devkit_path, 'bottle')
         self._classes = ('__background__', # always index 0
-                         'aeroplane', 'bicycle', 'bird', 'boat',
-                         'bottle', 'bus', 'car', 'cat', 'chair',
-                         'cow', 'diningtable', 'dog', 'horse',
-                         'motorbike', 'person', 'pottedplant',
-                         'sheep', 'sofa', 'train', 'tvmonitor')
+                         'ColaCanBlack', 'ColaCanRed', 'ColaCanWhite',
+                        'ColaPotBlack', 'ColaPotRed', 'ColaPotWhite',
+                        'FantaYellow', 'Sprite')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
